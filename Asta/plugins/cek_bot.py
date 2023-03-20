cad8a477e6be4b28e0e8c92e1d84c3412bb17079
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-20 22:59:49 +0800

    Create cek_bot.py

---
+++ Asta/plugins/cek_bot.py
@@ -0,0 +1,34 @@
+from pyrogram import filters, enums
+from pyrogram.errors import FloodWait 
+from app import bot,prefix
+import asyncio 
+
+from Asta.func.cek_admin import admins_only,bot_admin
+from Asta.func.pv_or_gc import no_private
+from Asta.func.error import error
+
+
+@bot.on_message(filters.command("bots",prefix) & ~filters.private) 
+@no_private 
+@bot_admin
+@admins_only 
+@error
+async def list_bots(client, message):  
+  try:
+    botList = []
+    async for bots in bot.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.BOTS):
+      botList.append(bots.user)
+    lenBotList = len(botList) 
+    text3  = f"**BOT LIST - {message.chat.title}**\n\n🤖 Bots\n"
+    while len(botList) > 1:
+      bots = botList.pop(0)
+      text3 += f"├ {bots.mention}\n"    
+    else:    
+      bots = botList.pop(0)
+      text3 += f"└ {bots.mention}\n\n"
+      text3 += f"✅ | **Total number of bots**: {lenBotList}"
+      await message.reply(text=text3) 
+  except Exception as e:
+    await message.reply(e,quote=True)
+
+
