7f3f44bd280e72ff580c7c1cbdb47afea9f4dfce
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-20 10:04:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from config import bot 
+from flask import Flask
+from threading import Thread 
+from pyrogram import idle
+import random
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def flask_run():
+  return "BOT RUN"
+
+
+def run_flask():
+  app.run(host="0.0.0.0",port=random.randint(2000,9000))
+
+def run_thread():
+  Thread(target=run_bot).start()
+
+def run_all():
+  run_thread()
+  bot.start()
+  idle()
+  bot.stop()
+
+run_all()
