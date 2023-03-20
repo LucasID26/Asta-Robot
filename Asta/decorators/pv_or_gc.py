dca62da0a200f3fea1af83ea857ec9e2afccdf85
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-20 13:07:43 +0800

    Create pv_or_gc.py

---
+++ Asta/decorators/pv_or_gc.py
@@ -0,0 +1,17 @@
+def no_private(func):
+  @wraps(func)
+  async def pv(client,m,*args,**kwargs):
+    if m.chat.type.value == 'private':
+      return await m.reply_text("Perintah ini dibuat untuk digunakan di obrolan grup, bukan di pm!",quote=True)
+    else:
+      return await func(client, m, *args, **kwargs)
+  return pv
+
+def no_group(func):
+  @wraps(func)
+  async def pv(client,m,*args,**kwargs):
+    if m.chat.type.value != 'private':
+      return await m.reply_text("Perintah ini dibuat untuk digunakan di pm, bukan di obrolan group!",quote=True)
+    else:
+      return await func(client, m, *args, **kwargs)
+  return pv
