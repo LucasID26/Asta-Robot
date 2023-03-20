a806cf250c0b0070250aba058e58c52df9a66f65
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-20 13:25:56 +0800

    Create file_size.py

---
+++ Asta/func/file_size.py
@@ -0,0 +1,12 @@
+SIZE_UNITS = ["B", "KB", "MB", "GB", "TB", "PB"]
+def file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return "0B"
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f"{round(size_in_bytes, 2)}{SIZE_UNITS[index]}"
+    except IndexError:
+         return "🤷‍♂️"
