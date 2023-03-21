2daa96cd9f940b41e2a5024039ff5de5a0163c09
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-21 10:40:23 +0800

    Create filters_db.py

---
+++ database/filters_db.py
@@ -0,0 +1,39 @@
+from typing import Dict, List, Union
+from config import dbname
+
+db = dbname.filters
+
+async def _get_filters(chat_id: int) -> Dict[str, int]:
+    filters = await filtersdb.find_one({"chat_id": chat_id})
+    return filters["filters"] if filters else {}
+
+async def del_filter(chat_id: int, name: str) -> bool:
+    filtersd = await _get_filters(chat_id)
+    name = name.lower().strip()
+    if name in filtersd:
+        del filtersd[name]
+        await filtersdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"filters": filtersd}},
+            upsert=True,
+        )
+        return True
+    return False 
+
+async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    return _filters[name] if name in _filters else False
+
+async def get_filters_names(chat_id: int) -> List[str]:
+    return list(await _get_filters(chat_id))
+
+async def save_filter(chat_id: int, name: str, _filter: dict):
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    _filters[name] = _filter
+    await filtersdb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"filters": _filters}},
+        upsert=True,
+    )
