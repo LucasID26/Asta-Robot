939e619a5bb007e4b801bca728ce4a047c196178
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-23 19:58:18 +0800

    Create debug.py

---
+++ Asta/plugins/debug.py
@@ -0,0 +1,26 @@
+import os
+from config import bot,own,prefix
+from pyrogram import filters
+
+from Asta.decorators.info_cmd import info_cmd
+from Asta.decorators.cek_admin import bot_admin
+from Asta.decorators.error import error
+
+
+
+@bot.on_message(filters.command('debug',prefix))
+@info_cmd
+@bot_admin
+@error
+async debug_cmd(client,m):
+  if m.reply_to_message is not None:
+    debug = m.reply_to_message
+  else:
+    debug = m
+  hasil = f"<b>HASIL-DEBUG:</b>\n<code>{debug}</code>"
+  if len(hasil) >= 4096:
+    with open("Asta_Debug.txt","w+",encoding="utf8") as dbg:
+      dbg.write(hasil)
+    return await m.reply_document("Asta_Debug.txt",caption="<b>HASIL-DEBUG</b>")
+  else:
+    return m.reply_text(hasil)
