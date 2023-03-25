569f34848bf7434979f1ccf1f4ef97e84deb22e5
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-25 18:10:15 +0800

    Update translate.py

--- Asta/func/translate.py
+++ Asta/func/translate.py
@@ -6,4 +6,7 @@ def translate(kode,query):
   res = get(url)
   bf = BF(res.content,'html.parser')
   hasil = bf.find("div",class_="w-full h-full bg-gray-100 h-full border p-2 min-h-25vh sm:min-h-50vh whitespace-pre-wrap break-words")
-  return hasil
+  if len(hasil) == 0:
+    return "Ahh Kode bahasa itu tidak ada bre"
+  else:
+    return hasil
