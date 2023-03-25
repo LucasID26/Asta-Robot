69ec30a1a1f45d7af3b17f7727387e66fa744c66
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-25 17:01:01 +0800

    Update translate.py

--- Asta/plugins/translate.py
+++ Asta/plugins/translate.py
@@ -8,17 +8,23 @@ from Asta.decorators.error import error
 
 
 @bot.on_message(filters.command(['tr','trans','translate'],prefix))
+@info_cmd
+@bot_admin
+@error
 async def trans(client,m):
   if len(m.command) == 1:
     return await m.reply_text("Google Translate,/tr [kode bhs] [text]")
   try:
-    if m.reply_to_message is not None:
+    if m.reply_to_message:
       text = m.reply_to_message.text 
     else:
       text = m.text.split(" ",2)[2]
     code = m.text.split(" ",1)[1]
-    tr = translate(code,text)
-    hasil = f"**Hasil translate ke {code}\n`{tr}`"
-    await m.reply_text(hasil)
+    try:
+      tr = translate(code,text)
+      hasil = f"**Hasil translate ke {code}\n\n`{tr}`"
+      await m.reply_text(hasil)
+    except:
+      await m.reply_text("Pastikan format sudah benar")
   except:
     await m.reply_text("Terjadi kesalahan")
