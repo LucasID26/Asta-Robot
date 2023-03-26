436cffe0a4e3afda9811bdc4ae5f31232758d098
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-26 16:37:59 +0800

    Update tools.py

--- Asta/func/tools.py
+++ Asta/func/tools.py
@@ -1,4 +1,25 @@
+import asyncio
+import math
 import os
+import shlex
+from typing import Tuple
+from PIL import Image
+
+
+
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
 
 def get_arg(m):
   msg = m.text
@@ -45,3 +66,50 @@ def resize_image(image):
   if os.path.exists(image):
     os.remove(image)
   return file_name
+
+
+
+
+async def resize_media(media: str, video: bool, fast_forward: bool) -> str:
+    if video:
+        info_ = Media_Info.data(media)
+        width = info_["pixel_sizes"][0]
+        height = info_["pixel_sizes"][1]
+        sec = info_["duration_in_ms"]
+        s = round(float(sec)) / 1000
+
+        if height == width:
+            height, width = 512, 512
+        elif height > width:
+            height, width = 512, -1
+        elif width > height:
+            height, width = -1, 512
+
+        resized_video = f"{media}.webm"
+        if fast_forward:
+            if s > 3:
+                fract_ = 3 / s
+                ff_f = round(fract_, 2)
+                set_pts_ = ff_f - 0.01 if ff_f > fract_ else ff_f
+                cmd_f = f"-filter:v 'setpts={set_pts_}*PTS',scale={width}:{height}"
+            else:
+                cmd_f = f"-filter:v scale={width}:{height}"
+        else:
+            cmd_f = f"-filter:v scale={width}:{height}"
+        fps_ = float(info_["frame_rate"])
+        fps_cmd = "-r 30 " if fps_ > 30 else ""
+        cmd = f"ffmpeg -i {media} {cmd_f} -ss 00:00:00 -to 00:00:03 -an -c:v libvpx-vp9 {fps_cmd}-fs 256K {resized_video}"
+        _, error, __, ___ = await run_cmd(cmd)
+        os.remove(media)
+        return resized_video
+
+    image = Image.open(media)
+    maxsize = 512
+    scale = maxsize / max(image.width, image.height)
+    new_size = (int(image.width * scale), int(image.height * scale))
+
+    image = image.resize(new_size, Image.LANCZOS)
+    resized_photo = "sticker.png"
+    image.save(resized_photo)
+    os.remove(media)
+    return resized_photo
