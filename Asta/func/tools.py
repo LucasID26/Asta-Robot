7139d2e424da21368c905708db023e9ab8bb7e62
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-26 16:16:24 +0800

    Update tools.py

--- Asta/func/tools.py
+++ Asta/func/tools.py
@@ -1,3 +1,5 @@
+import os
+
 def get_arg(m):
   msg = m.text
   msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
@@ -17,3 +19,29 @@ def get_text(m):
       return None
   else:
     return None
+
+def resize_image(image):
+  im = Image.open(image)
+  maxsize = (512, 512)
+  if (im.width and im.height) < 512:
+    size1 = im.width
+    size2 = im.height
+    if im.width > im.height:
+      scale = 512 / size1
+      size1new = 512
+      size2new = size2 * scale
+    else:
+      scale = 512 / size2
+      size1new = size1 * scale
+      size2new = 512
+    size1new = math.floor(size1new)
+    size2new = math.floor(size2new)
+    sizenew = (size1new, size2new)
+    im = im.resize(sizenew)
+  else:
+    im.thumbnail(maxsize)
+  file_name = "Sticker.png"
+  im.save(file_name, "PNG")
+  if os.path.exists(image):
+    os.remove(image)
+  return file_name
