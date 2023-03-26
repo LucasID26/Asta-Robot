e1d2701d1c3bc5d6525b4ce428056f802163e1c1
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-26 22:35:06 +0800

    Update quotly.py

--- Asta/plugins/quotly.py
+++ Asta/plugins/quotly.py
@@ -28,12 +28,13 @@ async def quotly(client,m):
     #await m.reply_to_message.forward(Bot)
     await asyncio.sleep(5)
     async for quotly in asisstant.search_messages(Bot, limit=1):
-      if quotly:
+      try:
+        asyncio.sleep(2)
         await msg.delete()
         rep = m.id if m.reply_to_message.from_user.is_bot == True else m.reply_to_message.id
         await m.reply_sticker(
                     sticker=quotly.sticker.file_id,
                     reply_to_message_id=rep)
-      else:
+      except:
         return await m.edit("**Gagal Membuat Sticker Quotly**")
 
