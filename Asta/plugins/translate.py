d5ba13a4bb296ff954ddfbaf41046105758b3421
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-28 12:55:00 +0800

    Update translate.py

--- Asta/plugins/translate.py
+++ Asta/plugins/translate.py
@@ -1,28 +1,27 @@
 from config import bot,prefix
 from pyrogram import filters
 
-from Asta.func.translate import translate
+from Asta.func.translate import trbali
 from Asta.decorators.info_cmd import info_cmd
 from Asta.decorators.cek_admin import bot_admin
 from Asta.decorators.error import error
 
 
-@bot.on_message(filters.command(['tr','trans','translate'],prefix))
+@bot.on_message(filters.command('trbali',prefix))
 @info_cmd
 @bot_admin
 @error
 async def trans(client,m):
   if len(m.command) == 1:
-    return await m.reply_text("Google Translate,/tr [kode bhs] [text]")
+    return await m.reply_text("Google Translate Bhs Bali, /bali [text]")
   try:
     if m.reply_to_message is not None:
       if not m.reply_to_message.text:
         return await m.reply_text("Reply pesan bukan media")
       text = m.reply_to_message.text 
     else:
-      text = m.text.split(" ",2)[2]
-    code = m.text.split(" ",2)[1]
-    tr = translate(code,text)
+      text = m.text.split(" ",1)[1]
+    tr = trbali(text)
     hasil = f"**Hasil translate ke {code}**\n\n`{tr}`"
     await m.reply_text(hasil)
   except:
