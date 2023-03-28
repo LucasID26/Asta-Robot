b4e6acf567e043f35cfbd5276583c512aec68cf4
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-28 12:55:44 +0800

    Update translate.py

--- Asta/func/translate.py
+++ Asta/func/translate.py
@@ -1,8 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup as BF
 
-def translate(kode,query):
-  url = f"https://translate.glosbe.com/id-{kode}/{query}"
+def translate(query):
+  url = f"https://translate.glosbe.com/id-ban/{query}"
   res = get(url)
   bf = BF(res.content,'html.parser')
   hasil = bf.find("div",class_="w-full h-full bg-gray-100 h-full border p-2 min-h-25vh sm:min-h-50vh whitespace-pre-wrap break-words")
