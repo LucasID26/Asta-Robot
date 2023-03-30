fd5e6b831539400414ba299d58e4f2e27b0dfb9a
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-03-30 14:27:25 +0800

    Update tiktok_dl.py

--- Asta/plugins/tiktok_dl.py
+++ Asta/plugins/tiktok_dl.py
@@ -17,7 +17,7 @@ link_data = {}
 @info_cmd
 @bot_admin 
 @error
-async def yt_dlyt_dl(client,m):
+async def tiktok_dl(client,m):
   link = m.text.split(" ",1)
   id = m.from_user.id
   if len(m.command) == 1:
@@ -29,8 +29,8 @@ async def yt_dlyt_dl(client,m):
   msg = await m.reply_text("`prosesss`")
   button = InlineKeyboard(row_width=2)
   button.add(
-    InlineButton('Video',callback_data=f'video_tt|{id}'),
-    InlineButton('Audio',callback_data=f'audio_tt|{id}')
+    InlineButton('Video',callback_data=f'tiktok|video_tt|{id}'),
+    InlineButton('Audio',callback_data=f'tiktok|audio_tt|{id}')
   )
   try:
     req = requests.get(f"https://apimu.my.id/downloader/tiktok3?link={link[1]}").json()
@@ -71,14 +71,14 @@ async def yt_dlyt_dl(client,m):
   except Exception as e: 
     await msg.edit(f"ERORR : {e}")
 
-@bot.on_callback_query(group=3)
-async def callback_dl(client,call):
+@bot.on_callback_query(filters.create(lambda _, __, query: "tiktok" in query.data))
+async def callback_dl_tt(client,call):
   data = call.data.split("|",1)
   id = str(call.from_user.id)
   try:
-    if data[1] == id:
+    if data[2] == id:
       msg = await bot.edit_message_caption(chat_id=call.message.chat.id,message_id=call.message.id,caption=f"📥 **Mengunduh**")    
-      if data[0] == 'video_tt': 
+      if data[1] == 'video_tt': 
         title = link_data[id]['v_title']
         like = link_data[id]['like']
         comment = link_data[id]['comment']
@@ -98,7 +98,7 @@ async def callback_dl(client,call):
 """)
         await msg.delete()
         os.remove(title)
-      elif data[0] == 'audio_tt':
+      elif data[1] == 'audio_tt':
         title = link_data[id]['a_title'] + '.mp3'
         author = link_data[id]['author']
         like = link_data[id]['like']
