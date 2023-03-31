a774c9611040a2eded154075177499c3f6f33f08
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-01 01:08:34 +0800

    Update monitoring.py

--- Asta/plugins/monitoring.py
+++ Asta/plugins/monitoring.py
@@ -27,12 +27,9 @@ async def save_monitor(client,m):
 @bot.on_message(filters.command("monitor",prefix) & filters.user(own[0]))
 async def get_monitor(client,m):
   msg = await m.reply_text("`Mengumpulkan data monitor . . .`")
-  hasil = ""
-  valid = "**STATISTIK MONITOR**\n"
-  invalid = "**URL TIDAK VALID**\n"
-  j_valid = 0
-  j_invalid = 0
-  a = 1
+  if len(db.find_one({"name":"MONITOR"})['monitor_url']) == 0:
+    return await msg.edit("**MONITOR KOSONG**")
+  valid = "**STATISTIK MONITOR**\n\n"
   for url in db.find_one({"name":"MONITOR"})['monitor_url']:
     name = urlparse(url).netloc.split(".",1)[0]
     try:
@@ -46,20 +43,9 @@ async def get_monitor(client,m):
         status_c = res.status_code
         respon = "Tidak ada respon"
       valid += f"**Name:** `{name}`\n**Status:** `{status}`\n**Status Code:** `{status_c}`\n**Response:** `{respon}`\n\n"
-      j_valid += 1
     except:
-      invalid += f"{a}. {url}\n"
-      j_invalid += 1
-      a += 1
-  if j_valid and j_invalid != 0:
-    hasil += valid + invalid
-  elif j_valid and j_invalid == 0:
-    hasil += "Monitor Kosong"
-  elif j_valid != 0 and j_invalid == 0:
-    hasil += valid
-  elif j_invalid != 0 and j_valid == 0:
-    hasil += invalid
-  await msg.edit(hasil)
+      valid += f"**URL INVALID:** {url}\n\n"
+  await msg.edit(valid + invalid)
 
 @bot.on_message(filters.command("delmonitor",prefix) & filters.user(own[0]))
 async def del_monitor(client,m):
