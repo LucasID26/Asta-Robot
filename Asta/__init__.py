08c1ea1b30a22ff76d514f303d35ee1d126a78e3
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-11 15:24:54 +0800

    Update __init__.py

--- Asta/__init__.py
+++ Asta/__init__.py
@@ -1,1 +1,21 @@
 from . import plugins
+
+import importlib
+import os
+
+def check_plugins():
+  with open('Asta/plugins/__init__.py', 'r') as f:
+    init_lines = f.readlines()
+
+  teks = ""
+  for file in os.listdir('Asta/plugins'):
+    if file.endswith('.py') and not file.startswith('__'):
+      module_name = file[:-3]
+      if module_name not in sys.modules:
+        importlib.import_module(module_name)
+        teks += f"- `{module_name}`\n")
+        import_line = f"from . import {module_name}\n"
+        if import_line not in init_lines:
+          with open('Asta/plugins/__init__.py', 'a') as f:
+            f.write(import_line)
+  return teks
