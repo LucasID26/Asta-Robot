fe6cdd914f45d2208328b77b77f45173e6946aa1
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-11 15:39:46 +0800

    Update __init__.py

--- Asta/__init__.py
+++ Asta/__init__.py
@@ -2,6 +2,8 @@ from . import plugins
 
 import importlib
 import os
+from config import bot,prefix,own
+from pyrogram import filters
 
 def check_plugins():
   with open('Asta/plugins/__init__.py', 'r') as f:
@@ -19,3 +21,15 @@ def check_plugins():
           with open('Asta/plugins/__init__.py', 'a') as f:
             f.write(import_line)
   return teks
+
+
+@bot.on_message(filters.command('restart))
+async def restart_plugins(client,m):
+  teks = "**NEW PLUGINS :**\n"
+  msg = await m.reply_text("__Restarting plugins__. . .")
+  plugins = check_plugins()
+  if len(plugins) == 0:
+    return await msg.edit("Tidak ada plugins baru!")
+  teks += f"{plugins}\n\n__**Restarting berhasil✅**__"
+  await msg.edit(teks)
+
