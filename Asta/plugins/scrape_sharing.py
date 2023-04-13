d2c603564b91b498acf8613d9691a947586ee901
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 00:48:11 +0800

    Create scrape_sharing.py

---
+++ Asta/plugins/scrape_sharing.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from config import asisstant
+
+import asyncio
+
+@asisstant.on_message(filters.chat(-1001627097162))
+async def sharing(client,id):
+  text = m.text
+  urls = []
+  words = text.split()
+  for word in words:
+    url_components = urlparse(word)
+    if url_components.scheme and url_components.netloc:
+        urls.append(word)
+  for url in urls:
+    if urlparse(url).path in ['/ADITXROBOT']:
+      split_url = urlparse(url).query.split("=")[1]
+      query = f"/start {split_url}"
+      bot = str(urlparse(url).path.replace('/',''))
+      await asisstant.send_message(bot,query)
+      await asyncio.sleep(5)
+      async for message in asisstant.get_chat_history(bot,limit=1):
+        if message.video:
+        Id = message.video.file_id
+        await asisstant.send_video("aslibukansuci",video=Id)
