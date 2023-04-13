9a854208633718c912439c4eab690ceaf050ee15
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-13 14:19:35 +0800

    Create __main__.py

---
+++ Asta/__main__.py
@@ -0,0 +1,106 @@
+from config import bot,prefix,own 
+from pyrogram import filters
+import subprocess
+import sys
+import os
+import re 
+import pickle
+
+@bot.on_message(filters.command('restart',prefix) & filters.user(own[0]))
+async def restart_plugins(client,m):
+  msg = await m.reply_text("__Restarting BOT__. . .")
+  with open("restart.pickle", "wb") as status:
+    pickle.dump([m.chat.id, msg.id], status)
+  restarting() 
+
+def restarting():
+  restart_program()
+  menulis_init()
+  if os.path.exists("restart.pickle"):
+    with open('restart.pickle', 'rb') as status:
+      chat_id, message_id = pickle.load(status)
+      os.remove("restart.pickle")
+    pesan = ""
+    pesan_awal = "__Restarting BOT__. . .\n"
+    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n`Menginstall requirements. . .`")
+    install = install_requirements()
+    pesan += install
+    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n{pesan}\n\n`Mengecek file yang diupdate. . .`")
+    pull = git()
+    pesan += pull
+    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan}\n\n`Restarting berhasil✅`")
+
+def restart_program():
+  os.system('git pull')
+  os.execv(sys.executable, ['python'] + sys.argv)
+
+   
+def menulis_init():
+  for file in os.listdir('Asta/plugins'):
+    with open('Asta/plugins/__init__.py', 'r') as f:
+      init_lines = f.readlines()
+    if file.endswith('.py') and not file.startswith('__'):
+      module_name = file[:-3]
+      import_line = f"from . import {module_name}"
+      if not re.search(fr"\b{re.escape(import_line)}\b", str(init_lines)):
+        with open('Asta/plugins/__init__.py', 'a') as f:
+          f.write("\n"+import_line)
+
+
+def install_requirements():
+  completed_process = subprocess.run(['pip', 'install', '-r', 'requirements.txt'], capture_output=True)
+  
+  result_dict = {}
+  if completed_process.returncode == 0:
+    result_dict['status'] = 'Berhasil✅'
+    result_dict['message'] = 'Semua module berhasil diinstal'
+  else:
+    result_dict['status'] = 'Kegagalan❎'
+    result_dict['message'] = 'Beberapa modul gagal diinstal'
+    result_dict['failed_modules'] = []
+    for line in completed_process.stderr.splitlines():
+      line = line.decode('utf-8').strip()
+      if line.startswith('ERROR: Could not find a version'):
+        module_name = line.split()[6]
+        result_dict['failed_modules'].append(module_name)
+  pesan_install = f"**Install Requirements :**\n"
+  if result_dict['status'] == 'Berhasil✅':
+    pesan_install += f"**Status :** {result_dict['status']}\n**INFO :** {result_dict['message']}"
+  else:
+    pesan_install += f"**Status :** {result_dict['status']}\n**INFO :** {result_dict['message']}\n**Module :** {'\n'.join(['  - ' + module for module in result_dict['failed_modules']])}"
+  return pesan_install
+
+
+def git():
+  #subprocess.run(['git', 'pull'])
+  git_diff = subprocess.Popen(['git', 'diff', '--name-status', 'HEAD@{1}..HEAD'], stdout=subprocess.PIPE)
+  output_str = git_diff.communicate()[0].decode('utf-8')
+
+  added_files = []
+  modified_files = []
+  deleted_files = []
+  hasil = ""
+  for line in output_str.split('\n'):
+    if line.startswith('A'):
+      added_files.append(line[2:])
+    elif line.startswith('M'):
+      modified_files.append(line[2:])
+    elif line.startswith('D'):
+      deleted_files.append(line[2:])
+  if added_files:
+    hasil += '**FILE BARU :**\n'
+    for file in added_files:
+      hasil += file+'\n'
+    pass 
+  if modified_files:
+    hasil += '**FILE DI EDIT :**\n'
+    for file in modified_files:
+      hasil += file+'\n'
+  if deleted_files:
+    hasil += '**FILE DIHAPUS :**\n'
+    for file in deleted_files:
+      hasil += file+'\n'
+  if not added_files and not modified_files and not deleted_files:
+    hasil += "**UP TO DATE WITH MAIN**"
+  return hasil
+      
