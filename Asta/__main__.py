b6efa6b67b095ef8bc64b098665800a0fa7ccbb8
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-13 14:32:16 +0800

    Update __main__.py

--- Asta/__main__.py
+++ Asta/__main__.py
@@ -11,7 +11,7 @@ async def restart_plugins(client,m):
   msg = await m.reply_text("__Restarting BOT__. . .")
   with open("restart.pickle", "wb") as status:
     pickle.dump([m.chat.id, msg.id], status)
-  restarting() 
+  await restarting() 
 
 async def restarting():
   restart_program()
@@ -20,15 +20,15 @@ async def restarting():
     with open('restart.pickle', 'rb') as status:
       chat_id, message_id = pickle.load(status)
       os.remove("restart.pickle")
-    pesan = ""
-    pesan_awal = "__Restarting BOT__. . .\n"
-    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n`Menginstall requirements. . .`")
-    install = install_requirements()
-    pesan += install
-    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n{pesan}\n\n`Mengecek file yang diupdate. . .`")
-    pull = git()
-    pesan += pull
-    await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan}\n\n`Restarting berhasil✅`")
+  pesan = ""
+  pesan_awal = "__Restarting BOT__. . .\n"
+  await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n`Menginstall requirements. . .`")
+  install = install_requirements()
+  pesan += install
+  await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan_awal}\n{pesan}\n\n`Mengecek file yang diupdate. . .`")
+  pull = git()
+  pesan += pull
+  await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"{pesan}\n\n`Restarting berhasil✅`")
 
 def restart_program():
   os.system('git pull')
