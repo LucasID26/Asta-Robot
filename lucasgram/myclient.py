ea3cafd42396f9d061848cd1d7dbe31ee3b2b2e2
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 00:20:05 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-from method.graph import CreateTextGraph
+from .method.graph import CreateTextGraph
   
 class MyClient(Client):
   async def CreateTextGraph(self, title: str, content: list):
