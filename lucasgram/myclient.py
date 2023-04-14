b9d7562d724c12574ffa51495d02d21f949517f8
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 00:02:19 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -4,18 +4,9 @@ from typing import List
 
   
 class MyClient(Client):
-  async def create_graph(self, title: str, content):
+  async def CreateTextGraph(self, title: str, content: list):
     telegraph = Telegraph()
     telegraph.create_account(short_name='my_account')
-
-    if isinstance(content, str):  # jika content berupa path file
-      with open(content, "rb") as f:
-        file_bytes = f.read()
-      image_url = upload_file(file_bytes)
-      html_content = f'<img src="{image_url.link}"/>'
-    else:
-      html_content = content
-
-    page = telegraph.create_page(title=title, html_content=html_content)
+    page = telegraph.create_page(title=title, html_content=''.join(content))
     return page['url']
 
