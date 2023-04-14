b9d8d2fc72b2cd02d2986550f49291aa0b431c14
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 15:23:30 +0800

    Update monitoring.py

--- Asta/plugins/monitoring.py
+++ Asta/plugins/monitoring.py
@@ -31,12 +31,12 @@ async def save_monitor(client,m):
         "friendly_name": urlparse(url).netloc
   }
   res = requests.post(url_uptime, data=payload).json()
-  status = "Berhasil✅" if res['stat'] == "ok" else "Kegagalan❎"
+  stat = "Berhasil✅" if res['stat'] == "ok" else "Kegagalan❎"
   if status == 'Berhasil✅':
-    pesan = f"**Nama :** `{urlparse(url).netloc}`\n**Status :** `{status}`\n**ID :** `{res['monitor']['id']}`"
+    pesan = f"**Nama :** `{urlparse(url).netloc}`\n**Stat :** `{stat}`\n**Status :** `{res['monitor']['status']}`\n**ID :** `{res['monitor']['id']}`"
     await m.reply_text(pesan)
   elif status == 'Kegagalan❎':
-    pesan = f"**Status :** `{status}`\n**Error :** `{res['error']['message']}`" 
+    pesan = f"**Stat :** `{stat}`\n**Error :** `{res['error']['message']}`" 
     await m.reply_text(pesan)
 
 
@@ -47,40 +47,40 @@ async def get_monitor(client,m):
   msg = await m.reply_text("`Mengumpulkan data monitor . . .`")
   if len(db.find_one({"name":"MONITOR"})['monitor_url']) == 0:
     return await msg.edit("**MONITOR KOSONG**")
-  valid = "**STATISTIK MONITOR**\n\n"
-  for url in db.find_one({"name":"MONITOR"})['monitor_url']:
-    name = urlparse(url).netloc.split(".",1)[0]
-    try:
-      res = requests.get(url)
-      if res.status_code == 200:
-        status = "Aktif"
-        status_c = res.status_code
-        respon = res.text
-      else:
-        status = "Tidak Aktif"
-        status_c = res.status_code
-        respon = "Tidak ada respon"
-      valid += f"**Name:** `{name}`\n**Status:** `{status}`\n**Status Code:** `{status_c}`\n**Response:** `{respon}`\n\n"
-    except:
-      valid += f"**URL INVALID:** {url}\n\n"
-  await msg.edit(valid)
+  
+  pesan = "**STATISTIK MONITOR**\n\n"
+  for monitor in get_monitor():
+    nama = monitor['friendly_name']
+    status = monitor['status']
+    id = monitor['id']
+    interval = monitor['interval']
+    timeout = monitor['timeout']
+    pesan += f"**Nama :** `{nama}`\n**Status :** `{status}`\n**ID :** `{id}`\n**Interval :** `{interval}`\n**Timeout :** `{timeout}`\n\n"
+  await msg.edit(pesan)
 
 @bot.on_message(filters.command("delmonitor",prefix) & filters.user(own[0]))
 async def del_monitor(client,m):
   if len(m.command) == 1:
-    return await m.reply_text("Cantumkan URL yang mau dihapus dari daftar monitor")
-  url = m.text.split(" ",1)[1]
-  if not urlparse(url).scheme:
-    return await m.reply_text("Cantumkan URL yang valid")
-  key = {"name": "MONITOR"}
-  if db.find_one(key):
-    if url in db.find_one(key)['monitor_url']:
-      db.update_one(key,{"$pull":{"monitor_url": url}})
-      await m.reply_text("URL dihapus dari daftar monitor")
-    else:
-      await m.reply_text("URL tidak ada dalam daftar monitor saya!")
-  else:
-    await m.reply_text("Monitor kosong")
+    return await m.reply_text("Cantumkan ID monitor yang mau dihapus dari daftar monitor")
+  id = m.text.split(" ",1)[1]
+  if not id.isdigit():
+    return await m.reply_text("Cantumkan ID berupa integer bukan string")
+  url_uptime = "https://api.uptimerobot.com/v2/deleteMonitor"
+  payload = {
+    "api_key": api_key,
+    "format": "json",
+    "type": 1,
+    "id": id
+  }
+
+  res = requests.post(url_uptime, data=payload).json()
+  stat = "Berhasil✅" if res['stat'] == "ok" else "Kegagalan❎"
+  if status == 'Berhasil✅':
+    pesan = f"**Nama :** `{urlparse(url).netloc}`\n**Stat :** `{stat}`\n**Status :** `{res['monitor']['status']}`\n**ID :** `{res['monitor']['id']}`"
+    await m.reply_text(pesan)
+  elif status == 'Kegagalan❎':
+    pesan = f"**Stat :** `{stat}`\n**Error :** `{res['error']['message']}`" 
+    await m.reply_text(pesan)
 
 
 
