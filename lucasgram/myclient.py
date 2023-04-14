2b3aa8b523e809cbf8b1ac32cc3e37c35261bac6
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 23:28:15 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -3,16 +3,21 @@ from telegraph import Telegraph,upload_file
 from typing import List
 
 class MyClient(Client):
-  async def create_graph(self, title: str, content: str, photo_path: str = None):
+  
+class MyClient(Client):
+  async def create_graph(self, title: str, content):
     telegraph = Telegraph()
     telegraph.create_account(short_name='my_account')
 
-    if photo_path:
-      with open(photo_path, 'rb') as file:
-        photo_url = upload_file(file)['src']
-      content.insert(0, f'<img src="{photo_url}"/>')
-
-    html_content = ''.join(content)
+    if isinstance(content, str):  # jika content berupa teks
+      html_content = content
+    elif isinstance(content, str):  # jika content berupa path file
+      with open(content, 'rb') as f:
+        img = f.read()
+      image_url = upload_image(img)
+      html_content = f'<img src="{image_url}"/>'
+    else:
+      raise ValueError('Content harus berupa teks atau path file')
 
     page = telegraph.create_page(title=title, html_content=html_content)
     return page['url']
