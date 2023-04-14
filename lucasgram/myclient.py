cb56f3a99499b9333cb5d6579f348835040a6209
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 00:15:24 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -1,12 +1,8 @@
 from pyrogram import Client
-from telegraph import Telegraph,upload_file
-from typing import List
 
+from method.telegraph import CreateTextGraph
   
 class MyClient(Client):
   async def CreateTextGraph(self, title: str, content: list):
-    telegraph = Telegraph()
-    telegraph.create_account(short_name='my_account')
-    page = telegraph.create_page(title=title, html_content=''.join(content))
-    return page['url']
+    await CreateTextGraph(self, title: str, content: list)
 
