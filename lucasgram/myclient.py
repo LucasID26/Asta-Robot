5fefd3bf3aecdd2eb90b8298e7f500961c80f17b
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 23:24:10 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -3,7 +3,7 @@ from telegraph import Telegraph,upload_file
 from typing import List
 
 class MyClient(Client):
-  async def create_graph(self, title: str, content: List[str], photo_path: str = None):
+  async def create_graph(self, title: str, content: str, photo_path: str = None):
     telegraph = Telegraph()
     telegraph.create_account(short_name='my_account')
 
