734a929022402affa846e8ef28b8e3920401dd7d
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 13:42:38 +0800

    Update sendGmail.py

--- Asta/func/sendGmail.py
+++ Asta/func/sendGmail.py
@@ -3,7 +3,7 @@ import smtplib
 from email.mime.text import  MIMEText
 from email.mime.multipart import MIMEMultipart
 
-asta_gmail = os.environ["ASTA_GMAIL"]
+asta_gmail = os.environ["BOT_GMAIL"]
 g_pass = os.environ["GMAIL_PASS"]
 owner_gmail = os.environ["OWN_GMAIL"]
 
