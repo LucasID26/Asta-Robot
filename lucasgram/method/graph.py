80cc74c0e245ffd5c1c01b3d8ede799be1195342
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 00:34:10 +0800

    Delete lucasgram/method directory

--- lucasgram/method/graph.py
+++
@@ -1,7 +0,0 @@
-fron telegraph import Telegraph
-
-async def CreateTextGraph(self, title: str, content: list):
-  telegraph = Telegraph()
-  telegraph.create_account(short_name='my_account')
-  page = telegraph.create_page(title=title, html_content=''.join(content))
-  return page['url']
