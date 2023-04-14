83e3ac943afcb1478e631290cb217b9ea00fcac8
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-14 14:40:48 +0000

    Update

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -1,35 +1,10 @@
-from pyrogram import Client, Message
-from typing import List
+from pyrogram import Client
+from telegraph import Telegraph
 
 
 class MyClient(Client):
-    async def build_table(self, judul: List[str], query: List[str]) -> str:
-        # menghitung panjang maksimal untuk setiap kolom berdasarkan isi data
-        max_lengths = [len(max(judul, key=len))] + [len(max(query[i], key=len)) for i in range(len(judul))]
-
-        # membangun header
-        header = '|'.join([f'{judul[i]:^{max_lengths[i]}}' for i in range(len(judul))])
-        header += '\n' + '|'.join(['-' * length for length in max_lengths])
-
-        # membangun baris data
-        rows = ''
-        for i in range(len(query[0])):
-            row = '|'.join([f'{query[j][i]:^{max_lengths[j]}}' for j in range(len(judul))])
-            rows += '\n' + row
-
-        # mengembalikan teks tabel
-        return f'{header}\n{rows}'
-
-    async def send_table(self, chat_id: int, judul: List[str], query: List[str]):
-        # membangun teks tabel menggunakan method build_table
-        table_text = await self.build_table(judul, query)
-
-        # kirim pesan dengan teks tabel menggunakan fungsi send_message
-        await self.send_message(chat_id, table_text, parse_mode='Markdown')
-
-    async def reply_table(self, message: Message, judul: List[str], query: List[str]):
-        # membangun teks tabel menggunakan method build_table
-        table_text = await self.build_table(judul, query)
-
-        # kirim pesan dengan teks tabel menggunakan fungsi reply
-        await message.reply(table_text, parse_mode='Markdown')
+  async def create_graph(self, title: str, content: list):
+    telegraph = Telegraph()
+    telegraph.create_account(short_name='my_account')
+    page = telegraph.create_page(title=title, html_content=''.join(content))
+    return page['url']
