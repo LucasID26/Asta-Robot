26ed2541d59c51467387fbd32d7cf0850e0c671c
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 11:56:25 +0800

    Update GetAllProfile.py

--- lucasgram/methods/GetAllProfile.py
+++ lucasgram/methods/GetAllProfile.py
@@ -1,18 +1,19 @@
 from pyrogram.types import InputMediaPhoto, InputMediaVideo 
 
-async def get_all_profile(client, chat_id: int, user_id: int, inputphoto: bool = True, inputvideo: bool = True):
+@staticmethod
+async def get_all_profile(chat_id: int, user_id: int, inputphoto: bool = True, inputvideo: bool = True):
   media = []
   if inputphoto:
-    photos = await client.get_profile_photos(user_id)
+    photos = await MyClient.get_profile_photos(user_id)
     media.extend(photos)
   if inputvideo:
-    videos = await client.get_profile_videos(user_id)
+    videos = await MyClient.get_profile_videos(user_id)
     media.extend(videos)
-      
+
   for item in media:
     if isinstance(item, dict) and item.get("file_id") is not None:
-      await client.send_photo(chat_id, photo=item["file_id"])
+      await MyClient.send_photo(chat_id, photo=item["file_id"])
     elif isinstance(item, dict) and item.get("video_file_id") is not None:
-      await client.send_video(chat_id, video=item["video_file_id"])
+      await MyClient.send_video(chat_id, video=item["video_file_id"])
     else:
       print("Tipe media tidak didukung")
