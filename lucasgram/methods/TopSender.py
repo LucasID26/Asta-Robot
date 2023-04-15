626926eb442607440b2b9cbdde6626911160b10d
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 13:09:28 +0800

    Update TopSender.py

--- lucasgram/methods/TopSender.py
+++ lucasgram/methods/TopSender.py
@@ -1,18 +1,18 @@
 import itertools
 
-async def GetTopSender(self, chat_id):
+async def GetTopSender(self, chat_id, max_senders=10):
   try:
     senders = {}
     async for message in self.search_messages(chat_id=chat_id):
       user_id = message.from_user.id
       senders[user_id] = senders.get(user_id, 0) + 1
 
-    sorted_senders = sorted(senders.items(), key=lambda x: x[1], reverse=True)
-    top_senders = itertools.islice(sorted_senders, 10)
+    top_senders = dict(sorted(senders.items(), key=lambda x: x[1], reverse=True)[:max_senders])
 
-    top_senders_info = {await self.get_users(sender[0]): sender[1] for sender in top_senders}
+    top_senders_info = {await self.get_users(sender_id): senders[sender_id] for sender_id in top_senders.keys()}
 
     return top_senders_info
 
   except Exception as e:
     print(f"Error getting top senders: {e}")
+
