f79f9a8433209e74e6e9f7c685e7a28137dead03
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-16 00:46:17 +0800

    Update SendTable.py

--- lucasgram/methods/SendTable.py
+++ lucasgram/methods/SendTable.py
@@ -15,4 +15,11 @@ async def SendTable(self, chat_id, data, caption=None, message_thread_id: int = 
     plt.savefig(buffer, format='png')
     buffer.seek(0)
 
-    await self.send_photo(chat_id=chat_id, photo=buffer, caption=caption, message_thread_id=message_thread_id)
+    if caption is not None and message_thread_id is not None:
+      await self.send_photo(chat_id=chat_id, photo=buffer, caption=caption, reply_to_message_id=message_thread_id)
+    elif caption is not None:
+      await self.send_photo(chat_id=chat_id, photo=buffer, caption=caption)
+    elif message_thread_id is not None:
+      await self.send_photo(chat_id=chat_id, photo=buffer, reply_to_message_id=message_thread_id)
+    else:
+      await self.send_photo(chat_id=chat_id, photo=buffer)
