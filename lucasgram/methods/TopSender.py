e117299c94bd07fc796efb1f192a8bc22fe84af4
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 13:19:38 +0800

    Update TopSender.py

--- lucasgram/methods/TopSender.py
+++ lucasgram/methods/TopSender.py
@@ -1,18 +1,29 @@
 import itertools
 
-async def GetTopSender(self, chat_id, max_senders=10):
+async def GetTopSender(self, chat_id):
   try:
+    #members_count = await self.get_chat_members_count(chat_id)
+    #all_members = await self.get_chat_members(chat_id, limit=members_count)
+
     senders = {}
     async for message in self.search_messages(chat_id=chat_id):
       user_id = message.from_user.id
       senders[user_id] = senders.get(user_id, 0) + 1
 
-    top_senders = dict(sorted(senders.items(), key=lambda x: x[1], reverse=True)[:max_senders])
+    top_sender_id = max(senders, key=senders.get)
+    top_sender = await self.get_users(top_sender_id)
 
-    top_senders_info = {await self.get_users(sender_id): senders[sender_id] for sender_id in top_senders.keys()}
+    result_dict = {
+        "top_sender": {
+            "first_name": top_sender.first_name,
+            "message_count": senders[top_sender_id]
+        },
+        "all_senders": {
+            await self.get_users(sender_id): message_count for sender_id, message_count in senders.items()
+        }
+    }
 
-    return top_senders_info
+    return result_dict
 
   except Exception as e:
-    print(f"Error getting top senders: {e}")
-
+    print(f"Error getting top sender: {e}")
