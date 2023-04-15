7f3ba8a2edd7d7318d785d5398d7d3f32e79feee
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 12:29:47 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -1,22 +1,12 @@
 from pyrogram import Client
 import os
 import importlib
-
+from . import methods
 
 
 class MyClient(Client):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-
-    methods_dir = "lucasgram/methods"
-
-    for filename in os.listdir(methods_dir):
-      if filename.endswith(".py"):
-        module_name = filename[:-3]
-        module = importlib.import_module(f"lucasgram.methods.{module_name}")
-
-        for name in dir(module):
-          if not name.startswith("__"):
-            method = getattr(module, name)
-            setattr(self, name, method)
+  async def create_text_graph(self, title: str, content: list):
+    return await CreateTextGraph(title, content)
+  async def get_top_sender(self, chat_id):
+    return await GetTopSender(self, chat_id)
 
