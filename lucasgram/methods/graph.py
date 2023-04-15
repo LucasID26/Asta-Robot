3e5bdd070e208c927d010ae5cba7aec5610ace29
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 11:57:22 +0800

    Update graph.py

--- lucasgram/methods/graph.py
+++ lucasgram/methods/graph.py
@@ -1,7 +1,8 @@
 from telegraph import Telegraph
 
 
-async def create_text_graph(self, title: str, content: list):
+@staticmethod
+async def create_text_graph(title: str, content: list):
   telegraph = Telegraph()
   telegraph.create_account(short_name='my_account')
   page = telegraph.create_page(title=title, html_content=''.join(content))
