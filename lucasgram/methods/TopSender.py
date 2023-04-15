3c6d7dec78d989a75ee1b7446d211477f5a39dbd
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 14:41:28 +0800

    Update TopSender.py

--- lucasgram/methods/TopSender.py
+++ lucasgram/methods/TopSender.py
@@ -1,21 +1,18 @@
 import itertools
 import json
 
-async def GetTopSender(self, chat_id):
+async def GetTopSender(self, chat_id, limit=10):
   try:
     senders = {}
     async for message in self.search_messages(chat_id=chat_id):
       user_id = message.from_user.id
       senders[user_id] = senders.get(user_id, 0) + 1
 
-    top_sender_id = max(senders, key=senders.get)
-    top_sender = await self.get_users(top_sender_id)
+    sorted_senders = sorted(senders.items(), key=lambda x: x[1], reverse=True)[:limit]
 
-    all_senders = []
-    for sender_id, message_count in senders.items():
+    top_senders = []
+    for sender_id, message_count in sorted_senders:
       user = await self.get_users(sender_id)
-      if len(all_senders) >= 10:
-        break
       user_dict = {
           "id": user.id,
           "first_name": user.first_name,
@@ -23,24 +20,15 @@ async def GetTopSender(self, chat_id):
           "username": user.username,
           "message_count": message_count
       }
-      all_senders.append({
+      top_senders.append({
           "user": user_dict
       })
 
     result_dict = {
-        "top_sender": {
-            "user": {
-                "id": top_sender.id,
-                "first_name": top_sender.first_name,
-                "last_name": top_sender.last_name,
-                "username": top_sender.username,
-                "message_count": senders[top_sender_id]
-            }
-        },
-        "all_senders": all_senders
+        "top_senders": top_senders
     }
 
     return json.dumps(result_dict, indent=2)
 
   except Exception as e:
-    print(f"Error getting top sender: {e}")
+    print(f"Error getting top senders: {e}")
