9422606707d77eeeed019fd26a4c59043a532eca
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 12:37:22 +0800

    Update TopSender.py

--- lucasgram/methods/TopSender.py
+++ lucasgram/methods/TopSender.py
@@ -1,15 +1,12 @@
-
-
 async def GetTopSender(self, chat_id):
   try:
     members_count = await self.get_chat_members_count(chat_id)
     all_members = await self.get_chat_members(chat_id, limit=members_count)
 
     senders = {}
-    for member in all_members:
-      user_id = member.user.id
-      messages = await self.search_messages(chat_id=chat_id, from_user=user_id)
-      senders[user_id] = messages.total
+    async for message in self.search_messages(chat_id=chat_id):
+      user_id = message.from_user.id
+      senders[user_id] = senders.get(user_id, 0) + 1
 
     top_sender_id = max(senders, key=senders.get)
     top_sender = await self.get_users(top_sender_id)
