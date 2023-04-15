56c8c6c79c07e39326d3b377427a94390fee1aef
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-15 10:51:13 +0800

    Update myclient.py

--- lucasgram/myclient.py
+++ lucasgram/myclient.py
@@ -3,8 +3,12 @@ from telegraph import Telegraph
   
 class MyClient(Client):
   #FUN
-  async def CreateTextGraph(self, title: str, content: list):
-    telegraph = Telegraph()
-    telegraph.create_account(short_name='my_account')
-    page = telegraph.create_page(title=title, html_content=''.join(content))
-    return page['url']
+  async def create_text_graph(self, title: str, content: list):
+    await CreateTextGraph(title, content)
+
+
+async def CreateTextGraph(title, content):
+  telegraph = Telegraph()
+  telegraph.create_account(short_name='my_account')
+  page = telegraph.create_page(title=title, html_content=''.join(content))
+  return page['url']
