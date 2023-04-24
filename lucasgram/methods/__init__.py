79557ee51ffd715f42bbfa8020ff16dffe6d8a69
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-24 08:35:21 +0800

    Update __init__.py

--- lucasgram/methods/__init__.py
+++ lucasgram/methods/__init__.py
@@ -1,6 +1,6 @@
-from . import method
+from . import methods
 
-from method.create_text_graph import CreateTextGraph
-from method.get_top_sender import GetTopSender
-from method.send_table import SendTable
-from method.get_top_admins import GetTopAdmins
+from methods.create_text_graph import CreateTextGraph
+from methods.get_top_sender import GetTopSender
+from methods.send_table import SendTable
+from methods.get_top_admins import GetTopAdmins
