1d8d2935b475a949366e166b3921e6725401825b
LucasID26 <128318440+LucasID26@users.noreply.github.com>
2023-04-25 06:48:19 +0800

    Update __init__.py

--- lucasgram/methods/__init__.py
+++ lucasgram/methods/__init__.py
@@ -1,4 +1,3 @@
 from lucasgram.methods.create_text_graph import CreateTextGraph
 from lucasgram.methods.get_top_sender import GetTopSender
 from lucasgram.methods.send_table import SendTable
-from lucasgram.methods.get_top_admins import GetTopAdmins
